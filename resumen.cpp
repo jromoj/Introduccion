20c4e38d0566dd080d17d302d22891a51fd823b9
Replit user <replituser@example.com>
2021-03-18 01:36:28 +0000

    notas v1

---
+++ resumen.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //std::
+#include<cmath>     //Funciones matematicas
+#include <cstdlib>  //Leer datos de consola
+
+/* 
+  for(start;end;increase)
+  std::atof(argv[])
+  std::cout
+  std::endl
+
+*/
